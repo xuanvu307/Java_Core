ebf5b3ee8ced5dcb637a0a6be745c34d4637989a
xuanvu307 <xuanvufb2@gmail.com>
2022-10-03 17:42:22 +0700

    a

---
+++ src/MaxAAndB.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class MaxAAndB {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input number a: ");
+        int a = sc.nextInt();
+        System.out.println("Input number b: ");
+        int b = sc.nextInt();
+        System.out.println("max a,b = " +Math.max(a,b));
+    }
+}
