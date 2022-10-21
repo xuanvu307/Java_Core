1d24ebf057946523c9131a6608483ca5d8dec6d5
xuanvu307 <xuanvufb2@gmail.com>
2022-10-22 00:00:10 +0700

    up

--- src/quanlyhocsinh/control/Main.java
+++ src/quanlyhocsinh/control/Main.java
@@ -12,6 +12,7 @@ public class Main {
         ArrayList<School> schools = new ArrayList<>();
         SchoolModel schoolModel = new SchoolModel();
 
+        // nhập vào số học sinh
 
         System.out.println("enter number student add");
         int numberStudent = Integer.parseInt(sc.nextLine());
@@ -21,18 +22,34 @@ public class Main {
             schools.add(school);
         }
 
+
+        // in ra những ai sinh năm 1985 và quê ở thái nguyên
+
+        int check = 0;
         System.out.println("student is birthday 1985 and address in thai nguyen");
         for (School school: schools){
             if(school.getStudent().getBirthday().getYear()==1985
                     && school.getStudent().getAddress().toLowerCase().equals("thai nguyen")){
                 System.out.println(school.getStudent().toString());
+                check++;
             }
         }
+        if (check == 0){
+            System.out.println("no student birthday 1985 and address in thai nguyen");
+        }
+
+        //in ra những học sinh học lớp 10A1
+
+        check = 0;
         System.out.println("\nstudent on class 10a1");
         for (School school: schools){
             if (school.getaClass().toLowerCase().equals("10a1")){
                 System.out.println(school.toString());
+                check++;
             }
+        }
+        if (check ==0 ){
+            System.out.println("no stdent on 10A1");
         }
     }
 }
